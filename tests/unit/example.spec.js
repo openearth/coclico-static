f6988f8582d1221f5efd552ae42190799c250898
Fedor Baart <fedor.baart@deltares.nl>
2022-03-02 14:28:08 +0100

    cleanup

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,10 +3,9 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
+    const msg = 'Welcome'
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
     })
-    expect(wrapper.text()).toMatch(msg)
+    expect(wrapper.text()).toContain(msg)
   })
 })
--- tests/unit/zarr.spec.js
+++ tests/unit/zarr.spec.js
@@ -6,7 +6,6 @@ describe('zarr', () => {
     it('opens', async () => {
         const z = await openArray({
             store: "https://storage.googleapis.com/hydro-engine-public/coclico/CoastAlRisk_Europe_ESL_Historical.zarr",
-            // store: "http://localhost:8080/waterlevel",
             path: "rp",
             mode: "r"
         });
