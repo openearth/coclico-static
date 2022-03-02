02b642c347cde1bf16b0e83ecea7d8c6dab93dca
IoannaMi1 <ioannamicha@hotmail.com>
2022-03-02 09:15:05 +0100

    Update env file; add vue-html-loader for loading icons

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,33 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  chainWebpack: config => {
+    config.module.rules.delete('svg')
+  },
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.svg$/,
+          loader: 'vue-html-loader'
+        },
+        {
+          test: /\.md$/,
+          use: [
+            {
+              loader: 'vue-html-loader'
+            },
+            {
+              loader: 'markdown-loader',
+              options: {
+                pedantic: true,
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+
 })
