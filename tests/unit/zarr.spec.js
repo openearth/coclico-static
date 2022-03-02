320cea511921ed17633856725efdaf5989d70048
Fedor Baart <fedor.baart@deltares.nl>
2022-03-02 16:24:00 +0100

    add 1 more test getting coordinates

--- tests/unit/zarr.spec.js
+++ tests/unit/zarr.spec.js
@@ -1,14 +1,36 @@
 import { slice, openArray } from "zarr";
+import _ from 'lodash'
 import 'whatwg-fetch'
 
 
 describe('zarr', () => {
     it('opens', async () => {
-        const z = await openArray({
+        const prArr = await openArray({
             store: "https://storage.googleapis.com/hydro-engine-public/coclico/CoastAlRisk_Europe_ESL_Historical.zarr",
             path: "rp",
             mode: "r"
         });
-        console.log(z);
+        expect(prArr).toBeDefined()
+
+    })
+    it('read latitude and longitude', async () => {
+        let store = "https://storage.googleapis.com/hydro-engine-public/coclico/CoastAlRisk_Europe_ESL_Historical.zarr"
+        const latArr = await openArray({
+            store: store,
+            path: "latitude",
+            mode: "r"
+        })
+        const lonArr = await openArray({
+            store: store,
+            path: "longitude",
+            mode: "r"
+        });
+
+        const lat = await latArr.get()
+        const lon = await lonArr.get()
+
+        const points = _.zip(lat.data, lon.data)
+        expect(points).toHaveLength(11014)
+
     })
 })
