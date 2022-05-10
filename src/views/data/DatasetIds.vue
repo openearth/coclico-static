60284b0513547d56ce6c201c975bb0dde0d546cd
IoannaMi1 <ioannamicha@hotmail.com>
2022-05-10 14:06:12 +0200

    first setup: different when clicking vector data
    *TODO: investigate why data route dont reset back

---
+++ src/views/data/DatasetIds.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+  import { mapActions } from 'vuex'
+
+  export default {
+    watch: {
+      '$route.params.datasetIds': {
+        handler () {
+          this.storeActiveVectorIds(this.$route.params.datasetIds)
+          //this.triggerActiveVector()
+        },
+        deep: true
+      }
+    },
+    mounted () {
+      this.storeActiveVectorIds(this.$route.params.datasetIds)
+      //this.triggerActiveVector()
+    },
+    methods: {
+      ...mapActions([ 'storeActiveVectorIds' ])
+    }
+  }
+</script>
+
+<style lang="css" scoped></style>
