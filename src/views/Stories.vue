eeb13838606a315345e34125ef7e2c265b965b86
EtienneKras <62947009+EtienneKras@users.noreply.github.com>
2022-09-21 09:41:40 +0200

    Revert "Update User Agreement & Fix linting problems"
    This reverts commit f62e2db415219bac21e69964d0e5888ee113fe92.

--- src/views/Stories.vue
+++ src/views/Stories.vue
@@ -7,61 +7,67 @@
             Stories
           </h2>
           <div v-for="theme in stories" :key="theme.id">
-            <stories-layout :theme="theme.theme" :stories="theme.stories" />
+             <stories-layout :theme="theme.theme" :stories="theme.stories"/>
           </div>
         </v-container>
       </v-col>
       <v-col cols="4" class="pa-4">
-        <v-container>
+         <v-container>
           <h2 class="h2">
             News
           </h2>
-          <news-layout :feeds="news" />
+          <news-layout :feeds="news"/>
         </v-container>
       </v-col>
     </v-row>
-    <v-img max-width="200px" contain class="background-logo" src="icon" />
+    <v-img
+      max-width="200px"
+      contain
+      class="background-logo"
+      src="icon"
+    />
   </v-flex>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
-import StoriesLayout from '@/components/StoriesLayout.vue'
-import NewsLayout from '@/components/NewsLayout.vue'
+  import { mapActions, mapGetters } from 'vuex'
+  import StoriesLayout from '@/components/StoriesLayout.vue'
+  import NewsLayout from '@/components/NewsLayout.vue'
 
-export default {
-  name: 'StoriesMenu',
-  components: {
-    StoriesLayout,
-    NewsLayout
-  },
-  mounted() {
-    this.loadNews()
-    this.loadStories()
-  },
-  computed: {
-    ...mapGetters(['stories', 'news']),
-    icon() {
-      return require('../assets/icons/icon-coclico.svg')
+  export default {
+    name: 'StoriesMenu',
+    components: {
+      StoriesLayout,
+      NewsLayout
+    },
+    mounted () {
+      this.loadNews()
+      this.loadStories()
+    },
+    computed: {
+      ...mapGetters(['stories', 'news']),
+      icon () {
+        return require('../assets/icons/icon-coclico.svg')
+      }
+    },
+    methods: {
+      ...mapActions(['loadNews', 'loadStories'])
     }
-  },
-  methods: {
-    ...mapActions(['loadNews', 'loadStories'])
   }
-}
-</script>
+  </script>
 
-<style scoped>
-.stories {
-  width: 100%;
-  height: 100%;
-  background-size: cover;
-  background-color: rgb(240, 240, 240);
-}
+  <style scoped>
+  .stories {
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-color: rgb(240,240,240);
+  }
 
-.background-logo {
-  position: absolute;
-  bottom: var(--spacing-default);
-  right: var(--spacing-default);
-}
+  .background-logo {
+    position: absolute;
+    bottom: var(--spacing-default);
+    right: var(--spacing-default);
+  }
+
 </style>
