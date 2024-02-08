ca6ceb223c3b0db7d501e883c8be8536710a8500
luisrodriguezgalvez <77997772+luisrodriguezgalvez@users.noreply.github.com>
2024-02-08 17:31:52 +0100

    started from scratch

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,240 +1,51 @@
 <template>
   <v-navigation-drawer
-    v-model="drawer"
-    mini-variant
-    mini-variant-width="200"
-    stateless
-    fixed
-    color="white"
+    permanent
     floating
-    class="app-sidebar"
-    style="border-right: 1px solid rgba(0, 0, 0, 0.12) !important"
-    v-show="!shouldHideDrawer"
+    rounded="xl"
+    width="200"
+    class="custom-navigation-drawer"
   >
-    <v-list dense class="pa-0" dark>
-      <v-list-item class="px-2" data-v-step="1">
-        <!-- above @click="$router.push({ name: 'home' })" -->
-        <!-- TODO: add routing -->
-        <!-- <custom-icon name="coclico-full" class="icon--large" /> -->
-        <v-list-item-content style="color: black">
-          <v-list-item-title> CoCliCo </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-divider />
-      <v-list-item-group>
-        <!-- above color="secondary" active-class="active-theme" -->
-        <v-list-item class="list-elements" style="color: black">
-          <!-- above v-for="item in getThemes"
-          :key="item"
-          @click="
-            toggleTheme(item);
-            openLayersCard();
-          "
-          :active="isActive(item)" -->
-          <div class="list-item">
-            <v-list-item-icon>
-              <custom-icon class="ml-8" />
-              <!-- above :name="item" icon-folder="themes"  -->
-            </v-list-item-icon>
-            <v-list-item-title>
-              <!-- above {{ item }} -->
-            </v-list-item-title>
-            Title BLA1
-          </div>
-          <v-list-item-content>
-            <v-list-item-title
-              >Title BLA2
-              <!-- above {{ item }} -->
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
+    <div class="image-container">
+      <img :src="coclicoIcon" alt="Coclico Icon" class="coclico-image" />
+    </div>
+    <v-list>
+      <v-list-item
+        prepend-icon="mdi-view-dashboard"
+        title="Dashboard"
+      ></v-list-item>
+      <v-list-item prepend-icon="mdi-account-box" title="Account"></v-list-item>
+      <v-list-item prepend-icon="mdi-gavel" title="Admin"></v-list-item>
     </v-list>
-    <v-row class="mt-10" :align="align" no-gutters>
-      <v-col>
-        <div class="list-item">
-          <v-btn icon color="black">
-            <v-icon>mdi-magnify</v-icon>
-          </v-btn>
-          <v-subtitle>
-            <!-- above class="text-caption" -->
-            Search
-          </v-subtitle>
-        </div>
-      </v-col>
-      <v-col>
-        <div class="list-item">
-          <v-btn icon color="black">
-            <!-- above @click="expandMenu = !expandMenu" -->
-            <v-icon>mdi-dots-horizontal</v-icon>
-          </v-btn>
-          <v-subtitle>
-            <!-- above v-show="expandMenu" class="text-caption" -->
-            Less
-          </v-subtitle>
-          <v-subtitle>
-            <!-- above v-show="!expandMenu" class="text-caption" -->
-            More
-          </v-subtitle>
-        </div>
-      </v-col>
-    </v-row>
-    <!-- <template #append>
-      <div v-show="expandMenu">
-        <v-list dense class="pa-0" color="terciary">
-          <v-list-item @click="openLandingPage">
-            <div class="extra-list-item">
-              <v-icon color="black" size="18px">mdi-information-outline</v-icon>
-              <v-list-item-subtitle class="extra-list-item-text"
-                >WEBSITE</v-list-item-subtitle
-              >
-            </div>
-            <v-list-item-content>
-              <v-list-item-title>Landing page</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item @click="openStoriesPage">
-            <div class="extra-list-item">
-              <v-icon color="black" size="18px">mdi-account-details</v-icon>
-              <v-list-item-subtitle class="extra-list-item-text"
-                >STORIES</v-list-item-subtitle
-              >
-            </div>
-            <v-list-item-content>
-              <v-list-item-title>Stories</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item @click="openCatalogPage">
-            <div class="extra-list-item">
-              <v-icon color="black" size="18px"> mdi-database-search </v-icon>
-              <v-list-item-subtitle class="extra-list-item-text"
-                >DATA CATALOG</v-list-item-subtitle
-              >
-            </div>
-            <v-list-item-content>
-              <v-list-item-title>Platform</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item @click="openWorkbenchPage">
-            <div class="extra-list-item-container">
-              <v-icon color="black" size="18px"> mdi-hammer </v-icon>
-              <v-list-item-subtitle class="extra-list-item-text"
-                >WORKBENCH</v-list-item-subtitle
-              >
-            </div>
-            <v-list-item-content>
-              <v-list-item-title>Workbench</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </div>
-    </template> -->
   </v-navigation-drawer>
 </template>
-<script>
-import CustomIcon from "@/components/CustomIcon";
-// import { mapGetters, mapMutations } from "vuex";
 
+<script>
 export default {
-  components: {
-    CustomIcon,
-  },
-  // computed: {
-  //   ...mapGetters(["getThemes", "getActiveTheme"]),
-  // },
   data() {
     return {
-      drawer: true,
-      mini: true,
-      activeTheme: null,
-      expandMenu: false,
-      shouldHideDrawer: false,
+      coclicoIcon: require("@/assets/icons/icon-coclico-full.svg"),
     };
   },
-  // methods: {
-  //   ...mapMutations(["toggleActiveTheme", "setShowLayersCardOpen"]),
-  //   openLayersCard(event) {
-  //     this.setShowLayersCardOpen();
-  //   },
-  //   openLandingPage() {
-  //     window.open("https://coclicoservices.eu", "_blank");
-  //   },
-  //   openWorkbenchPage() {
-  //     window.open("https://github.com/openearth/coclico-workbench", "_blank");
-  //   },
-  //   openStoriesPage() {
-  //     window.open(this.$router.resolve({ name: "stories" }).href, "_blank");
-  //   },
-  //   openCatalogPage() {
-  //     window.open(
-  //       "https://radiantearth.github.io/stac-browser/#/external/storage.googleapis.com/dgds-data-public/coclico/coclico-stac/catalog.json?.language=en",
-  //       "_blank"
-  //     );
-  //   },
-  //   toggleTheme(id) {
-  //     this.toggleActiveTheme(id);
-
-  //     if (this.activeTheme === id) {
-  //       this.activeTheme = null;
-  //     } else {
-  //       this.activeTheme = id;
-  //     }
-
-  //     this.$emit("change-theme");
-  //   },
-  //   isActive(id) {
-  //     return this.activeTheme === id;
-  //   },
-  //   checkRoute() {
-  //     this.shouldHideDrawer = this.$route.path.endsWith("/stories");
-  //   },
-  // },
-  //   created() {
-  //     this.checkRoute();
-  //   },
-  //   watch: {
-  //     $route(to) {
-  //       this.checkRoute();
-  //     },
-  //   },
 };
 </script>
 
-<style>
-.app-sidebar {
-  margin-top: var(--spacing-default);
-  margin-left: var(--spacing-default);
-  height: 100%;
-  max-height: calc(100% - 2 * (var(--spacing-default)));
+<style scoped>
+.custom-navigation-drawer {
+  margin-top: 50px;
+  margin-left: 50px;
+  max-height: calc(100% - 2 * (50px));
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
-
-.list-elements {
-  margin-top: 20px;
+.image-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-
-.list-item {
-  display: grid;
-  place-items: center;
-  text-align: center;
-}
-
-.extra-list-item {
-  display: grid;
-  place-items: center;
-  text-align: center;
-  margin-top: 9px;
-}
-
-.extra-list-item-container {
-  display: grid;
-  place-items: center;
-  text-align: center;
-  margin-top: 9px;
-  margin-bottom: 9px;
-}
-
-.extra-list-item-text {
-  font-size: 8px !important;
-  margin-top: 3px;
+.coclico-image {
+  width: 8rem;
+  height: 4rem;
 }
 </style>
