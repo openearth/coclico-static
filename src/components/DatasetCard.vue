9ea25ff72841c01982f2c426fbf5ae88a6b750d8
IoannaMi <ioannamicha@hotmail.com>
2024-03-26 15:50:30 +0100

    add separate tabs for Datasetcard; WIP ActiveDataset tab

--- src/components/DatasetCard.vue
+++ src/components/DatasetCard.vue
@@ -1,7 +1,12 @@
 <template>
+  <!-- TODO:
+        * Add custom icon compponent 
+        * Load correct icons
+        * Add colors based on the color library
+        * Replace option-1 or 2 in values
+ -->
   <v-card width="450px" height="397px" class="custom-dataset-card">
     <v-tabs v-model="tab" grow hide-slider class="pa-2" height="35px">
-      <!-- set as color primary the correct colors see old coclico implementation -->
       <v-tab
         value="option-1"
         hide-slider
@@ -17,7 +22,7 @@
         hide-slider
         rounded="lg"
         :ripple="false"
-        selected-class="test"
+        selected-class="selected-tab-style"
         class="mr-2"
       >
         <v-icon start> mdi-lock </v-icon>Dashboard</v-tab
@@ -25,42 +30,31 @@
     </v-tabs>
     <v-window v-model="tab">
       <v-window-item value="option-1">
-        <v-card flat>
-          <v-card-text>
-            <p>
-              Sed aliquam ultrices mauris. Donec posuere vulputate arcu. Morbi
-              ac felis. Etiam feugiat lorem non metus. Sed a libero.
-            </p>
-          </v-card-text>
-        </v-card>
+        <active-dataset-tab />
       </v-window-item>
       <v-window-item value="option-2">
-        <v-card flat>
-          <v-card-text>
-            Morbi nec metus. Suspendisse faucibus, nunc et pellentesque egestas,
-            lacus ante convallis tellus, vitae iaculis lacus elit id tortor. Sed
-            mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non
-            adipiscing dolor urna a orci. Curabitur ligula sapien, tincidunt
-            non, euismod vitae, posuere imperdiet, leo. Nunc sed turpis.
-          </v-card-text>
-        </v-card>
+        <dashboard-tab />
       </v-window-item>
     </v-window>
   </v-card>
 </template>
 <script>
+import ActiveDatasetTab from "./ActiveDatasetTab.vue";
+import DashboardTab from "./DashboardTab.vue";
+
 export default {
+  components: {
+    ActiveDatasetTab,
+    DashboardTab,
+  },
+
   data() {
     return {
       tab: "option-1",
       sliderValue: 0,
     };
   },
-  methods: {
-    selectMapElement() {
-      // Implement the functionality to select an element on the map
-    },
-  },
+  methods: {},
 };
 </script>
 
