d7260d73b57f6f6006c11329f7e2758556eee1a5
IoannaMi <ioannamicha@hotmail.com>
2024-05-31 17:06:06 +0200

    Read read dataset in active data layers

--- src/components/ActiveDatasetTab.vue
+++ src/components/ActiveDatasetTab.vue
@@ -1,34 +1,33 @@
 <template>
-  <v-card flat>
-    <v-container>
+  <v-card flat class="scrollable-card">
+    <v-container v-for="dataset in datasets" :key="dataset.id">
       <v-row>
-        <v-col cols="6">
+        <v-col cols="6" v-for="summary in dataset.summaries" :key="summary.id">
+          <v-row class="align-center">
+            <v-col cols="9" class="mr-0">
+              <span class="summary-info">{{ summary.id }}</span>
+            </v-col>
+            <v-col cols="3" class="pa-4">
+              <v-icon small class="summary-info"
+                >mdi-information-outline</v-icon
+              >
+            </v-col>
+          </v-row>
           <v-select
-            v-model="varA"
-            :items="['AR5']"
-            variant="outlined"
-          ></v-select>
-        </v-col>
-        <v-col cols="6">
-          <v-select v-model="varB" :items="['5']" variant="outlined"></v-select>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="12">
-          <v-select
-            v-model="varC"
-            :items="['Historical']"
+            v-model="summary.chosenValue"
+            :items="summary.allowedValues"
             variant="outlined"
           ></v-select>
         </v-col>
       </v-row>
-      <v-row>
+      <v-row class="pb-4">
         <v-col cols="12">
           <p class="text-style">
             Select an element in the map for specific location analysis.
           </p>
         </v-col>
       </v-row>
+      <v-divider />
     </v-container>
   </v-card>
 </template>
@@ -52,5 +51,12 @@ export default {
 <style>
 .text-style {
   background: #f0f0f0;
+}
+.scrollable-card {
+  max-height: 300px;
+  overflow-y: visible;
+}
+.summary-info {
+  color: #a9b0b5;
 }
 </style>
