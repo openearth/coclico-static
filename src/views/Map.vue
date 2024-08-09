5abfe937a3f003073bf7e38872c269801ef26991
luisrodriguezgalvez <77997772+luisrodriguezgalvez@users.noreply.github.com>
2024-08-09 11:50:49 +0200

    console warning fix
    getter alias in component

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -108,7 +108,7 @@ export default {
     ...mapGetters("map", [
       "mapboxLayers",
       "graphsInDashboard",
-      "seaLevelRiseData",
+      "seaLevelRiseDataFromStore:seaLevelRiseData", // Renamed getter
     ]),
   },
   created() {
