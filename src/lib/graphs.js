54379039f184f969681775c50c1cde621b70da09
Bazottie <basdegreeuw@voorhoede.nl>
2025-03-18 15:39:41 +0100

    Return null values for missing features

--- src/lib/graphs.js
+++ src/lib/graphs.js
@@ -143,6 +143,7 @@ async function exposed({ dataset, coords, props, fn }) {
   try {
     const { id } = dataset;
     const data = await fn(dataset, coords, props);
+    console.log(data);
     const scenarios = props.find(({ id }) => id === "scenarios").values;
     return {
       id,
